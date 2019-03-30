9354d8e224c1b48e80b87d7ba42b45f853ecee1f
lima-anika <limaanika58@gmail.com>
2019-03-31 01:23:02 +0600

    code

--- index.php
+++ index.php
@@ -142,9 +142,10 @@
 			<div class="block-slider block-slider4">
 				<ul class="" id="bxslider-home4">
 					<li>
-						<img src="img/s1.jpg" alt="Slide">
+						<img src="img/ss1.jpg" alt="Slide">
 						<div class="caption-group">
 							<h2 class="caption title">
+							
 								iPhone <span class="primary">6 <strong>Plus</strong></span>
 							</h2>
 							<h4 class="caption subtitle">Dual SIM</h4>
